71273fa961c31dc2b5b61110fe95a08c559b1285
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2026-02-18 21:49:42 -0300

    Primeiro Commit

---
+++ src/main/java/com/gustavocamargo/desafioremessa/DesafioremessaApplication.java
@@ -0,0 +1,13 @@
+package com.gustavocamargo.desafioremessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioremessaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioremessaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavocamargo/desafioremessa/DesafioremessaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavocamargo.desafioremessa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioremessaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
