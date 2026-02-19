ce71b6d90afd1e0765f8f593e4e820d1e25c0a12
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2026-02-19 14:41:03 -0300

    criacao classe usuario

---
+++ src/main/java/com/gustavocamargo/desafioremessa/infrastructure/persistence/entity/UsuarioEntity.java
@@ -0,0 +1,51 @@
+package com.gustavocamargo.desafioremessa.infrastructure.persistence.entity;
+
+import com.gustavocamargo.desafioremessa.domain.enums.TipoUsuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "usuarios")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String nomeCompleto;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String senha;
+    @Column(unique = true)
+    private String cpf;
+    @Column(unique = true)
+    private String cnpj;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoUsuario tipoUsuario;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dataCriacao;
+
+    private LocalDateTime dataAtualizacao;
+
+    @PrePersist
+    public void prePersist() {
+        this.dataCriacao = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.dataAtualizacao = LocalDateTime.now();
+    }
+
+}
