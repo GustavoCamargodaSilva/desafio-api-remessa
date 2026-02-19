bc8456ad10454f5a24cba69daa91196d615972eb
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2026-02-19 19:39:39 -0300

    request para criar usuario

---
+++ src/main/java/com/gustavocamargo/desafioremessa/application/dto/request/CriarUsuarioRequest.java
@@ -0,0 +1,17 @@
+package com.gustavocamargo.desafioremessa.application.dto.request;
+
+import com.gustavocamargo.desafioremessa.domain.enums.TipoUsuario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record CriarUsuarioRequest(
+        @NotBlank String nomeCompleto,
+        @Email @NotBlank String email,
+        @NotBlank String senha,
+        @CPF String cpf,
+        @CNPJ String cnpj,
+        @NotBlank TipoUsuario tipoUsuario
+) {
+}
