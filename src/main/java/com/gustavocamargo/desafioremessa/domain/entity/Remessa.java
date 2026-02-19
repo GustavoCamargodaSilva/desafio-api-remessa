814559cdc84b85755e5d9c9c2fd8fc91706ab3eb
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2026-02-19 10:55:18 -0300

    criaçao classe remessa

--- src/main/java/com/gustavocamargo/desafioremessa/domain/entity/Remessa.java
+++ src/main/java/com/gustavocamargo/desafioremessa/domain/entity/Remessa.java
@@ -2,127 +2,29 @@ package com.gustavocamargo.desafioremessa.domain.entity;
 
 import com.gustavocamargo.desafioremessa.domain.enums.Moeda;
 import com.gustavocamargo.desafioremessa.domain.enums.StatusRemessa;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Remessa {
 
     private UUID id;
     private String idempotencyKey;
     private UUID usuarioOrigemId;
     private UUID usuarioDestinoId;
-    private BigDecimal valorOrigem;
+    private BigDecimal valor;
     private Moeda moedaOrigem;
-    private Moeda moedaDestino;
     private BigDecimal valorConvertido;
+    private Moeda moedaDestino;
     private BigDecimal cotacaoUtilizada;
-
     private StatusRemessa status;
-    private LocalDateTime dataCriacao;
+    private LocalDateTime createdAt;
 
-    public Remessa() { }
-
-    public Remessa(UUID id, String idempotencyKey, UUID usuarioOrigemId, UUID usuarioDestinoId, BigDecimal valorOrigem, Moeda moedaOrigem, Moeda moedaDestino, BigDecimal valorConvertido, BigDecimal cotacaoUtilizada, StatusRemessa status, LocalDateTime dataCriacao) {
-        this.id = id;
-        this.idempotencyKey = idempotencyKey;
-        this.usuarioOrigemId = usuarioOrigemId;
-        this.usuarioDestinoId = usuarioDestinoId;
-        this.valorOrigem = valorOrigem;
-        this.moedaOrigem = moedaOrigem;
-        this.moedaDestino = moedaDestino;
-        this.valorConvertido = valorConvertido;
-        this.cotacaoUtilizada = cotacaoUtilizada;
-        this.status = status;
-        this.dataCriacao = dataCriacao;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getIdempotencyKey() {
-        return idempotencyKey;
-    }
-
-    public void setIdempotencyKey(String idempotencyKey) {
-        this.idempotencyKey = idempotencyKey;
-    }
-
-    public UUID getUsuarioOrigemId() {
-        return usuarioOrigemId;
-    }
-
-    public void setUsuarioOrigemId(UUID usuarioOrigemId) {
-        this.usuarioOrigemId = usuarioOrigemId;
-    }
-
-    public BigDecimal getValorOrigem() {
-        return valorOrigem;
-    }
-
-    public void setValorOrigem(BigDecimal valorOrigem) {
-        this.valorOrigem = valorOrigem;
-    }
-
-    public UUID getUsuarioDestinoId() {
-        return usuarioDestinoId;
-    }
-
-    public void setUsuarioDestinoId(UUID usuarioDestinoId) {
-        this.usuarioDestinoId = usuarioDestinoId;
-    }
-
-    public Moeda getMoedaOrigem() {
-        return moedaOrigem;
-    }
-
-    public void setMoedaOrigem(Moeda moedaOrigem) {
-        this.moedaOrigem = moedaOrigem;
-    }
-
-    public Moeda getMoedaDestino() {
-        return moedaDestino;
-    }
-
-    public void setMoedaDestino(Moeda moedaDestino) {
-        this.moedaDestino = moedaDestino;
-    }
-
-    public BigDecimal getValorConvertido() {
-        return valorConvertido;
-    }
-
-    public void setValorConvertido(BigDecimal valorConvertido) {
-        this.valorConvertido = valorConvertido;
-    }
-
-    public BigDecimal getCotacaoUtilizada() {
-        return cotacaoUtilizada;
-    }
-
-    public void setCotacaoUtilizada(BigDecimal cotacaoUtilizada) {
-        this.cotacaoUtilizada = cotacaoUtilizada;
-    }
-
-    public StatusRemessa getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusRemessa status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getDataCriacao() {
-        return dataCriacao;
-    }
-
-    public void setDataCriacao(LocalDateTime dataCriacao) {
-        this.dataCriacao = dataCriacao;
-    }
 }
