a2997a344a61aa7f327fca72df70f8a4180b215b
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2026-02-19 10:56:38 -0300

    limpando as classes com annotations spring

--- src/main/java/com/gustavocamargo/desafioremessa/domain/entity/Carteira.java
+++ src/main/java/com/gustavocamargo/desafioremessa/domain/entity/Carteira.java
@@ -1,11 +1,16 @@
 package com.gustavocamargo.desafioremessa.domain.entity;
 
 import com.gustavocamargo.desafioremessa.domain.enums.Moeda;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Carteira {
 
     private UUID id;
